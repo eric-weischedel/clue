d4cf77aefdfa3957bcfd0280929cee7c7d734ad6
DESKTOP-R57HG6C\eweis <ew1309@messiah.edu>
2020-03-02 19:41:42 -0500

    Changing probability generator to a class

--- algorithm/getCards.js
+++ algorithm/getCards.js
@@ -1,66 +1,81 @@
 import GameCards from '../assets/cards.js';
 import { readWorkingSave } from '../global/FileSystem.js';
 
+class ProbabilitiesMap {
+
+    cards;
+    myCards;
+    ruledOut;
+    map;
+
+    constructor(cards, myCards) {
+        this.cards = cards;
+        this.myCards = myCards;
+        this.ruledOut = 0;
+        this.objectify();
+        this.ruleOutMyCards();
+        this.setNonZeroCards();
+    }
+
+    getMap() {
+        return this.map.sort(function(a, b) {
+            return b.probability - a.probability;
+        });;
+    }
+
+    objectify() {
+        this.map = this.cards.map(value => (
+            {
+                name: value,
+                probability: null
+            }
+        ));
+    }
+
+    ruleOutMyCards() {
+        var vm = this;
+        this.map = this.map.map(function(value) {
+            if (vm.myCards.includes(value.name)) {
+                vm.ruledOut ++;
+                return {
+                    name: value.name,
+                    probability: 0
+                };
+            } else { return value; }
+        });
+    }
+
+    setNonZeroCards() {
+        var vm = this;
+        this.map = this.map.map(function(value) {
+            let p = 0;
+            if (value.probability === null) {
+                p = 1 / (vm.map.length - vm.ruledOut);
+                p = Math.round(p * 100);
+            }
+            return {
+                name: value.name,
+                probability: p
+            };
+        });
+    }
+}
+
 export async function getCardsAsync() {
     let file = await readWorkingSave();
     let save = JSON.parse(file);
     let myCards = save.myCards;
-    
-    // let myCards = {
-    //     suspects: [
-    //         'Colonel Mustard',
-    //         'Miss Scarlet',
-    //         'Mrs. Peacock',
-    //         'Mrs. White'
-    //     ],
-    //     weapons: [
-    //         'Candlestick',
-    //         'Wrench'
-    //     ],
-    //     rooms: [
-    //         'Ball Room',
-    //         'Billiard Room',
-    //         'Study'
-    //     ]
-    // };
 
-    let cards       = GameCards;
-    let suspects    = sortByP(setProbabilities(objectify(cards.suspects), myCards.suspects));
-    let weapons     = sortByP(setProbabilities(objectify(cards.weapons), myCards.weapons));
-    let rooms       = sortByP(setProbabilities(objectify(cards.rooms), myCards.rooms));
+    let cards = GameCards;
+
+    let suspects = new ProbabilitiesMap(cards.suspects, myCards.suspects);
+    let weapons = new ProbabilitiesMap(cards.weapons, myCards.weapons);
+    let rooms = new ProbabilitiesMap(cards.rooms, myCards.rooms);
 
     return {
-        suspects: suspects,
-        weapons: weapons,
-        rooms: rooms
+        suspects: suspects.getMap(),
+        weapons: weapons.getMap(),
+        rooms: rooms.getMap()
     };
-}
 
-function objectify(arr) {
-    return arr.map((value, index) => (
-        {
-            name: value,
-            probability: null
-        }
-    ));
-}
-
-function setProbabilities(arr, myCards) {
-    return arr.map(function(value, index) {
-        let p = 0;
-        if (!myCards.includes(value.name)) {
-            p = 1 / (arr.length - myCards.length);
-            p = Math.round(p * 100);
-        }
-        return {
-            name: value.name,
-            probability: p
-        };
-    });
-}
-
-function sortByP(arr) {
-    return arr.sort(function(a, b) {
-        return b.probability - a.probability;
-    });
 }
